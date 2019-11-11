346a1b98222a6c74011c5381ef28566fb0a97dfe
EBS-Robot <afeder@eaglebrook.org>
2019-11-11 13:23:13 -0500

    Feder motion updates: - linear backwards, forwards, sideways - rotate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FederTest.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by robotics on 2019-11-10.
+ * Testing if template will change
+ */
+
+@TeleOp(name = "FederTest", group = "Tests")
+public class FederTest extends LinearOpMode {
+
+    private DcMotor rightWheelFront;
+    private DcMotor rightWheelBack;
+    private DcMotor leftWheelFront;
+    private DcMotor leftWheelBack;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+        rightWheelFront = hardwareMap.dcMotor.get("rightWheel_1");
+        rightWheelBack = hardwareMap.dcMotor.get("rightWheel_2");
+        leftWheelFront = hardwareMap.dcMotor.get("leftWheel_1");
+        leftWheelBack = hardwareMap.dcMotor.get("leftWheel_2");
+
+        leftWheelFront.setDirection(DcMotor.Direction.REVERSE);
+        leftWheelBack.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            double rightStick_Y = -gamepad1.right_stick_y;
+            double rightStick_X = -gamepad1.right_stick_x;
+
+            double leftStick_Y = -gamepad1.left_stick_y;
+            double leftStick_X = -gamepad1.left_stick_x;
+
+
+            boolean bumper_Right = gamepad1.right_bumper;
+            boolean bumper_Left = gamepad1.left_bumper;
+
+
+            if(rightStick_X == 0 && rightStick_Y == 0 && leftStick_X == 0 && rightStick_Y == 0){
+                rightWheelBack.setPower(0);
+                rightWheelFront.setPower(0);
+                leftWheelBack.setPower(0);
+                leftWheelFront.setPower(0);
+            }
+            else if(rightStick_X > 0 && rightStick_Y == 0){
+                rightWheelBack.setPower(-rightStick_X);
+                rightWheelFront.setPower(rightStick_X);
+                leftWheelBack.setPower(rightStick_X);
+                leftWheelFront.setPower(-rightStick_X);
+            }
+            else if(rightStick_X < 0 && rightStick_Y == 0){
+                rightWheelBack.setPower(-rightStick_X);
+                rightWheelFront.setPower(rightStick_X);
+                leftWheelBack.setPower(rightStick_X);
+                leftWheelFront.setPower(-rightStick_X);
+            }
+            else if(rightStick_X == 0 && rightStick_Y != 0) {
+                rightWheelBack.setPower(rightStick_Y);
+                rightWheelFront.setPower(rightStick_Y);
+                leftWheelBack.setPower(rightStick_Y);
+                leftWheelFront.setPower(rightStick_Y);
+            }
+            else if(rightStick_X > 0 && rightStick_Y > 0){
+                rightWheelFront.setPower(rightStick_X);
+                leftWheelBack.setPower(rightStick_X);
+            }
+            else if(leftStick_X !=0){
+                rightWheelBack.setPower(leftStick_X);
+                rightWheelFront.setPower(leftStick_X);
+                leftWheelBack.setPower(-leftStick_X);
+                leftWheelFront.setPower(-leftStick_X);
+            }
+            idle();
+
+        }
+
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeanPark_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeanPark_OpMode.java
@@ -11,16 +11,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class SeanPark_OpMode extends OpMode {
     private DcMotor left_Wheel, right_Wheel;
-    private double motor_Power;
+    private double motor_Power_l, motor_Power_r;
 
     public void init(){
         left_Wheel = hardwareMap.dcMotor.get("left_motor");
         right_Wheel = hardwareMap.dcMotor.get("right_motor");
         right_Wheel.setDirection(DcMotorSimple.Direction.REVERSE); // right motor direction set
+
+        //set motors to 0 power
+        left_Wheel.setPower(0);
+        right_Wheel.setPower(0);
     }
+
     public void loop(){
-        motor_Power =  -gamepad1.left_stick_y;
-        left_Wheel.setPower(motor_Power);
-        right_Wheel.setPower(motor_Power);
+        motor_Power_l = -gamepad1.left_stick_y;
+        motor_Power_r = -gamepad1.right_stick_y;
+        left_Wheel.setPower(motor_Power_l);
+        right_Wheel.setPower(motor_Power_r);
     }
 }
